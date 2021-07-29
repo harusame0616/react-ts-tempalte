e49e3e04254da1230f93565b7935c87bf0b35a22
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2021-07-29 23:01:08 +0900

    chore: TSをトランスパイルできるように設定

--- webpack.config.ts
+++ webpack.config.ts
@@ -4,11 +4,20 @@ import * as webpack from 'webpack';
 
  const config: webpack.Configuration = {
     mode: 'development',
-    entry: './src/sample1/index.js',
+    entry: './src/sample1/index.ts',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
-    }
+     },
+     module: {
+         rules: [
+             {
+                 test: /\.(ts)$/,
+                 exclude: /node_modules/,
+                 use: "ts-loader",
+             }
+         ]
+     }
 }
 
-export default config;
+export default config
